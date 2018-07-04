216b3f80a39a9963d682b7e8b8f36624042ad86b
Katiuscia de Moraes Andrade <40796317+katiushademoraes@users.noreply.github.com>
2018-07-04 15:51:53 -0300

    Update diminutivo_split.py

--- exercicio-1/diminutivo_split.py
+++ exercicio-1/diminutivo_split.py
@@ -3,6 +3,7 @@
 # Módulo capaz de reconhecer, dentro de um texto, um diminutivo masculino padrão e converter para uma forma do falar nordestino.
 # Toqueniza sentenças, reconhecendo a pontuação final (tratamento primitivo por expressões regulares do Python). Não trata situações dúbias, tais como abreviações.
 # Criado a partir do módulo diminutivo_sentence
+# Por Katiuscia de Moraes
 import re
 def diminutivo_split():
     text: str = input("Digite um breve texto, contendo ao menos duas sentenças, onde haja algum diminutivo masculino na forma padrão, para convertê-lo(s) em um falar nordestino: ")
