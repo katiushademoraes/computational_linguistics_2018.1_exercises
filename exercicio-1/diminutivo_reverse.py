a77e42ee137bfdf0777419245fcf76c83be23fa6
Katiuscia de Moraes Andrade <40796317+katiushademoraes@users.noreply.github.com>
2018-07-05 17:41:30 -0300

    Create diminutivo_reverse.py

---
+++ exercicio-1/diminutivo_reverse.py
@@ -0,0 +1,37 @@
+#Módulo que retorna a forma do diminutivo padrão, a partir de um diminutivo masculino do falar nordestino
+
+
+def diminutivo_reverse():
+
+    reverse: str = input("Digite uma palavra, no diminutivo masculino do falar nordestino, para convertê-la na forma padrão: ")
+    for i in reverse:
+        if reverse[-2::] == "im":
+            word = reverse[0:-2]
+            word = ''.join((word, 'inho'))
+            dict=open("deadjectivals.mbr.dict.txt","r")
+            check=dict.read()
+            dict2= open("denominals.mbr.dict.txt", "r")
+            check2 = dict2.read()
+            if word in check or check2:
+                print("A forma padrão do diminutivo", reverse, "é", "'", word, "'")
+                break;
+            else:
+                print("A entrada não é um diminutivo.")
+                break;
+        else:
+            print("A entrada não é um diminutivo masculino do falar nordestino. Por favor, tente novamente.")
+            break;
+
+
+diminutivo_reverse()
+
+
+def next():
+    query = input("Deseja Prosseguir? [sim/não]: ")
+    while query in "sim":
+        diminutivo_word()
+        next()
+    if query not in "sim":
+        exit()
+
+next()
